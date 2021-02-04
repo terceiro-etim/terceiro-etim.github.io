5225cfff5eedee976d46cb90031438a519155ee5
ropoko <rodrigostramantinoli@gmail.com>
2021-02-04 15:17:43 -0300

    set template engine handlebars and folder structure

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const expbs = require('express-handlebars')
+
+app.engine('hbs', expbs({ defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/contato', (req, res) => {
+    res.render('contato')
+})
+
+app.get('/galeria', (req, res) => {
+    res.render('galeria')
+})
+
+app.listen(5500, () => {
+    console.log('working')
+})
