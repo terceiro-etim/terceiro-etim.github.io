5ce70cdaa83f7dfd28089d2e77d0de5ebfdf777e
Rikz1n <rique_moraes@hotmail.com>
2021-08-27 00:19:14 -0300

    Finalizada a Home

---
+++ index.js
@@ -0,0 +1,69 @@
+function prev() {
+    let slide = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slide.length; i++) {
+        if (window.getComputedStyle(slide[i]).order == 1) {
+            slide[i].style.order = 2;
+        }
+        else if (window.getComputedStyle(slide[i]).order == 2) {
+            slide[i].style.order = 3;
+        }
+        else {
+            slide[i].style.order = 1;
+        }
+    }
+
+    style();
+}
+
+function next() {
+    let slide = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slide.length; i++) {
+        if (window.getComputedStyle(slide[i]).order == 1) {
+            slide[i].style.order = 3;
+        }
+        else if (window.getComputedStyle(slide[i]).order == 2) {
+            slide[i].style.order = 1;
+        }
+        else {
+            slide[i].style.order = 2;
+        }
+    }
+
+    style();
+}
+
+function style() {
+    let slide = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slide.length; i++) {
+        if (window.getComputedStyle(slide[i]).order == 2) {
+            slide[i].style.zIndex = 10;
+            slide[i].style.opacity = "1";
+            slide[i].style.animation = "bb 0.2s ease-in-out 0.2s forwards";
+            slide[i].style.marginRight = "0";
+            slide[i].style.marginLeft = "0";
+            slide[i].style.height = "18rem";
+            slide[i].style.width = "18rem";
+        }
+        else if (window.getComputedStyle(slide[i]).order == 1) {
+            slide[i].style.zIndex = 0;
+            slide[i].style.marginRight = "-80px";
+            slide[i].style.marginLeft = "0";
+            slide[i].style.animation = "aa 0.2s ease-in-out 0.2s forwards";
+            slide[i].style.height = "15.3rem";
+            slide[i].style.width = "15.3rem";
+        }
+        else {
+            slide[i].style.zIndex = 0;
+            slide[i].style.marginLeft = "-80px";
+            slide[i].style.marginRight = "0";
+            slide[i].style.animation = "aa 0.2s ease-in-out 0.2s forwards";
+            slide[i].style.height = "15.3rem";
+            slide[i].style.width = "15.3rem";
+        }
+    }
+}
+
+window.onload = style;
